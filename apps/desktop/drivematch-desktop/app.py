01da700ed2e5fe2b5174eb1acecab80fb73b9a57
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-05-09 22:11:49 +0200

    better user feedback

--- apps/desktop/drivematch-desktop/app.py
+++ apps/desktop/drivematch-desktop/app.py
@@ -9,6 +9,7 @@ from urllib.parse import urlparse
 
 import platformdirs
 import os
+import logging
 
 from drivematch.service import (
     DriveMatchService, create_default_drivematch_service
@@ -17,12 +18,15 @@ from drivematch.service import (
 
 class DriveMatchDialog(QDialog):
     drive_match_service: DriveMatchService
+    logger: logging.Logger
+
     scrape_widget: ScrapeWidget
     analyze_widget: AnalyzeWidget
 
-    def __init__(self, drive_match_service: DriveMatchService, parent=None):
+    def __init__(self, drive_match_service: DriveMatchService, logger: logging.Logger, parent=None):
         super().__init__(parent)
         self.drive_match_service = drive_match_service
+        self.logger = logger
 
         self.setWindowTitle("Drive Match")
         self.setGeometry(200, 200, 1000, 600)
@@ -50,17 +54,15 @@ class DriveMatchDialog(QDialog):
         name = self.scrape_widget.get_name_text()
         url = self.scrape_widget.get_url_text()
 
-        if not name or not url:
-            print("Please enter a valid name and URL.")
-            return
-
         if not name.strip():
-            print("Name cannot be empty.")
+            self.logger.info("Got empty name: %s", name)
+            show_error_message("Please enter a valid name.")
             return
 
         parsed_url = urlparse(url)
-        if not parsed_url.scheme or not parsed_url.netloc:
-            print("Please enter a valid URL.")
+        if not url or not url.strip() or not parsed_url.scheme or not parsed_url.netloc:
+            self.logger.info("Got invalid url: %s", url)
+            show_error_message("Please enter a valid URL.")
             return
 
         self.scrape_widget.clear_name_text()
@@ -78,7 +80,8 @@ class DriveMatchDialog(QDialog):
     def set_scored_cars(self):
         selected_search_id = self.analyze_widget.get_selected_search_id()
         if selected_search_id is None:
-            print("Please select a search.")
+            self.logger.info("Got invalid selected search: %s", selected_search_id)
+            show_error_message("Please select a search.")
             return
         search_parameters = self.analyze_widget.get_search_parameters()
         scored_cars = self.drive_match_service.get_scores(
@@ -89,7 +92,8 @@ class DriveMatchDialog(QDialog):
     def set_grouped_cars(self):
         selected_search_id = self.analyze_widget.get_selected_search_id()
         if selected_search_id is None:
-            print("Please select a search.")
+            self.logger.info("Got invalid selected search: %s", selected_search_id)
+            show_error_message("Please select a search.")
             return
         grouped_cars = self.drive_match_service.get_groups(selected_search_id)
         self.analyze_widget.set_grouped_cars(grouped_cars)
@@ -100,20 +104,25 @@ def main():
     data_dir = platformdirs.user_data_dir(
         "DriveMatch", "DriveMatch", ensure_exists=True)
 
+    logger = logging.getLogger("DriveMatch")
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
+
     db_path = os.path.join(data_dir, "drivematch.db")
     if not os.path.exists(db_path):
+        logger.critical("DriveMatch database not found at %s", db_path)
         show_error_message("Cloud not find DriveMatch database at " + db_path)
         return
 
     drive_match_service = create_default_drivematch_service(db_path)
 
-    drive_match = DriveMatchDialog(drive_match_service)
+    drive_match = DriveMatchDialog(drive_match_service, logger)
     drive_match.show()
     app.exec()
 
 
 def show_error_message(message: str):
-    print(f"Error: {message}")
     msgBox = QMessageBox()
     msgBox.setText(message)
     msgBox.exec()
